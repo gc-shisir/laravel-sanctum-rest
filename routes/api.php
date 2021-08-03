fdf2d0c2a0594877656b3495b53222713e81acdb
Shisir GC <shisir.gc44@gmail.com>
2021-08-04 00:14:36 +0545

    added product search functionality

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('products','ProductController');
+Route::get('products/search/{name}',[ProductController::class,'search']);
 
 // Route::get('/products',[ProductController::class,'index']);
 
