f615184879afa7bf67030a4d3436b0eeda87abe9
Shisir GC <shisir.gc44@gmail.com>
2021-08-04 00:05:10 +0545

    completed product CRUD operation

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    protected $fillable=[
+        'name',
+        'slug',
+        'description',
+        'price'
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,18 +20,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products',function(){
-    return Product::all();
-});
+Route::resource('products','ProductController');
 
-Route::post('/products',function(){
-    return Product::create([
-        'name'=>'Product one',
-        'slug'=>'product-one',
-        'description'=>"this is product one",
-        'price'=>'99.99'
-    ]);
-});
+// Route::get('/products',[ProductController::class,'index']);
+
+// Route::post('/products',[ProductController::class,'store']);
 
 Route::get('/',function(){
     return ['method'=>'GET','url'=>'http://localhost:8000/api/products','message'=>'Fetch all products'];
